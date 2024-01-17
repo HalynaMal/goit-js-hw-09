67c94d754dcf35ba2a5e6be0ed6264c374e663e3
HalynaMal <142045196+HalynaMal@users.noreply.github.com>
2024-01-17 15:43:42 +0000

    Deploying to gh-pages from @ HalynaMal/goit-js-hw-09@607203b80b3fe215db58fc8a6396e38cbb299a89 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-7824baca.js";import{S as e}from"./assets/vendor-c9def49e.js";document.querySelector(".gallery");new e(".gallery a",{});
+import"./assets/styles-7824baca.js";import{S as e}from"./assets/vendor-c9def49e.js";document.querySelector(".gallery");new e(".gallery a",{captions:!0,captionsData:"alt",captionsDelay:250,fadeSpeed:250,captionPosition:"bottom",enableKeyboard:!0,close:!0,closeText:"x"});
 //# sourceMappingURL=commonHelpers.js.map
