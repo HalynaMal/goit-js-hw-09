fae312c118429a39f62391f9f486790ed63c1e28
HalynaMal <142045196+HalynaMal@users.noreply.github.com>
2024-01-17 15:20:50 +0000

    Deploying to gh-pages from @ HalynaMal/goit-js-hw-09@25f0bb603a290e82f70780146c86a1673273e421 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-7824baca.js";import{S as e}from"./assets/vendor-c9def49e.js";document.querySelector(".gallery");new e(".gallery a",{});
+//# sourceMappingURL=commonHelpers.js.map
